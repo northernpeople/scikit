5ca3903cba628ead6732ab1b3792caba49d12e07
Stepan Razdobarov <northernpeople@gmail.com>
2018-11-30 11:29:24 -0600

    added matrix

--- dec_tree.py
+++ dec_tree.py
@@ -1,9 +1,12 @@
 from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics
 import numpy as np
 from sklearn import datasets
 import matplotlib.pylab as plt
+import random
 
 iris = datasets.load_iris()
+
 X_train = iris.data[5:147]
 # print(X_train)
 
@@ -25,3 +28,15 @@ dtc.fit(X_train, Y_train)
 print("predicted: ", dtc.predict([X_test]))
 
 print(dtc.decision_path([X_test], check_input=True))
+
+
+
+# metrics and matrix
+yPred = dtc.predict(iris.data[1:140])
+
+mat = metrics.confusion_matrix(iris.target[1:140], yPred)
+print ("confusion matrix: \n", mat)
+
+
+print (metrics.classification_report(iris.target[1:140], yPred))
+print ("Overall Accuracy: ", round(metrics.accuracy_score(iris.target[1:140], yPred), 2))
